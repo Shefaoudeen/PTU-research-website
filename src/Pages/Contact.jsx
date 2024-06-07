8bb2d6db3e720523bcd72f361741247b056f994d
Pradheep Raj <pradheepselva111@gmail.com>
2024-06-07 18:48:05 +0530

    made Contact Us responsive

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
       <div className="">
         <h1 className="text-3xl font-bold py-10 ml-40">Contact Us</h1>
         {contact_Details.map((faculty,index) => {
-          return <div key={index} className={`flex items-center gap-16 pl-40 py-8 bg-[${faculty.bgColor}]`}>
+          return <div key={index} className={`flex flex-wrap items-center gap-16 justify-center md:justify-start md:pl-40 py-8 bg-[${faculty.bgColor}]`}>
               <div><img src={faculty.profile} alt="profile picture" className="w-52 rounded-full" /></div>
               <div className="pr-8">
                 <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.name}</h1>
-                <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.designation}</h1>
-                <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.clgName}</h1>
+                <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.designation},</h1>
+                <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.clgName}.</h1>
                 <h1 className={`text-2xl p-1 text-${faculty.textColor} `}>{faculty.email && `Email : ${faculty.email}`}</h1>
               </div>
           </div>;
