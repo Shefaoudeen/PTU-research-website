7ce0cc07c82eb4d865486175dca2c6d1f982161a
Pradheep Raj <pradheepselva111@gmail.com>
2024-06-08 15:41:34 +0530

    made Banner Responsive

--- src/Layout/Banner.jsx
+++ src/Layout/Banner.jsx
@@ -6,16 +6,16 @@ const Banner = (props) => {
   return (
     <>
       <div
-        className="h-[40vh] bg-no-repeat bg-cover"
+        className="h-[80vh] md:h-[40vh] bg-no-repeat bg-cover"
         style={{ backgroundImage: `url(${bannerBG})` }}
       >
-        <div className="flex items-center justify-between h-full  py-5 px-12">
+        <div className="flex flex-col md:flex-row items-center justify-evenly md:justify-between h-full  py-5 sm:px-12">
           <div>
             <img className="w-40" src={ptuLogo}></img>
           </div>
           <div className=" flex flex-col items-center gap-4 text-4xl px-10">
-            <h1 className="text-blue-300 font-semibold">{props.title}</h1>
-            <h1 className={`text-wrap text-2xl text-white ${props.desc_style}`}>
+            <h1 className="text-blue-300 font-semibold text-center">{props.title}</h1>
+            <h1 className={`text-wrap text-2xl text-white text-center ${props.desc_style}`}>
               {props.desc}
             </h1>
             <h1 className="text-wrap text-2xl text-white">
