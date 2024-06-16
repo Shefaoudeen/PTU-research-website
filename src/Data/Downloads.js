c830242c06175fe609fded7fefbcbc45b807515b
Pradheep Raj <pradheepselva111@gmail.com>
2024-06-16 13:17:20 +0530

    download over

---
+++ src/Data/Downloads.js
@@ -0,0 +1,54 @@
+import phd_regulation from '../assets/Images/docs-thumbnails/phd_regulation.png';
+import certificate_supervisor from '../assets/Images/docs-thumbnails/certificate_supervisor.png';
+import minutes_of_synopsis from '../assets/Images/docs-thumbnails/minutes_of_synopsis.png';
+import synopsis_form from '../assets/Images/docs-thumbnails/synopsis_form.png';
+import synopsis_covering from '../assets/Images/docs-thumbnails/synopsis_covering.png';
+import synopsis_notification from '../assets/Images/docs-thumbnails/synopsis_notification.png';
+import thesis_guidelines from '../assets/Images/docs-thumbnails/thesis_guidelines.png';
+import synopsis_attendance from '../assets/Images/docs-thumbnails/synopsis_attendance.png';
+
+const regulation = {
+    title : "Phd Regulations",
+    thumbnail : phd_regulation,
+    link : "https://drive.google.com/file/d/1N1e6coHxwFOGX_6rUXdQayuyvjllsk6T/view"
+}
+const thesis = {
+    title : "Thesis format Guidelines-version 1.0",
+    thumbnail : thesis_guidelines,
+    link : "https://drive.google.com/file/u/2/d/1-2y_xTAf0e2PELctd7UcBpZB83Rmkm0l/view"
+}
+
+const synopsisForms = [
+    {
+        title : "certificate_supervisor",
+        thumnail : certificate_supervisor,
+        link : "https://docs.google.com/document/d/1R5AowG6i4mA-ZeKhLO40oQ7vCwh5Kdne/edit?usp=drive_link&ouid=102544829276987280173&rtpof=true&sd=true"
+    },
+    {
+        title : "Minutes_of_synopsis_meeting",
+        thumnail : minutes_of_synopsis,
+        link : "https://docs.google.com/document/d/1tzmImzu1c125J-x7J7a8031fyPB4QRCQ/edit"
+    },
+    {
+        title : "PTU Synopsis Submission Form",
+        thumnail : synopsis_form,
+        link : "https://docs.google.com/document/d/112B8MUarJRSJOxfSn7Q8bv0E20uCwmMF/edit"
+    },
+    {
+        title : "PTU_Synopsis_submission_covering_letter",
+        thumnail : synopsis_covering,
+        link : "https://docs.google.com/document/d/1xMgdSAqZ3q5xmmNOH_VX8IxmVAhS4md4/edit"
+    },
+    {
+        title : "Synopsis_Attendance_Format",
+        thumnail : synopsis_attendance,
+        link : "https://docs.google.com/document/d/1epBC-HwvBeGAMc43530JEVXFMdxzgc5I/edit"
+    },
+    {
+        title : "Synopsis_Notification",
+        thumnail : synopsis_notification,
+        link : "https://docs.google.com/document/d/1vzinWA7lmhk1ucxsDZUs4zjEWGl-KeU1/edit"
+    },
+]
+
+export {regulation,thesis,synopsisForms}
--- src/Pages/Download.jsx
+++ src/Pages/Download.jsx
@@ -1,17 +1,70 @@
 import React from "react";
 import Banner from "../Layout/Banner";
+import { regulation, thesis, synopsisForms } from "../Data/Downloads.js";
 
 const Download = () => {
   return (
-    <div>
-      {" "}
+    <>
       <Banner
         title={"Directorate of Academic Research"}
         desc={"Downloads"}
         desc_style={"font-bold text-4xl"}
         additional={""}
       />
-    </div>
+      <div className=" p-20 flex justify-around flex-wrap gap-y-4">
+        <div className="flex flex-col items-center gap-4 ">
+          <div className="w-80 shadow-lg">
+            <img alt={regulation.title} src={regulation.thumbnail}></img>
+          </div>
+          <div className="flex flex-col  items-center">
+            <h2 className="text-2xl py-4 text-center">{regulation.title}</h2>
+            <a href={regulation.link}>
+              <h2 className="py-2 px-4 bg-blue-500 rounded-full text-xl text-white max-w-fit shadow-md hover:scale-105 duration-300">
+                Download
+              </h2>
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <div className="w-80 shadow-lg">
+            <img alt={thesis.title} src={thesis.thumbnail}></img>
+          </div>
+          <div className="flex flex-col  items-center">
+            <h2 className="text-2xl py-4 text-center">{thesis.title}</h2>
+            <a href={thesis.link}>
+              <h2 className="py-2 px-4 bg-blue-500 rounded-full text-xl text-white max-w-fit shadow-md hover:scale-105 duration-300">
+                Download
+              </h2>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h1 className="text-4xl text-blue-800 text-center font-semibold">
+          Synopsis Forms
+        </h1>
+        <div className="mt-10 grid grid-cols-2 gap-y-4 divide-x-2 grid-flow-column">
+          {synopsisForms.map((form, index) => {
+            return (
+              <div key={index} className={`py-4 px-10 flex flex-wrap justify-center gap-y-5 md:justify-between items-center bg-slate-100`}>
+                <div className="w-48 shadow-lg flex-shrink-0">
+                  <img className="rounded-md" src={form.thumnail} alt={form.title} />
+                </div>
+                <div className="flex flex-col items-center gap-4">
+                  <h2 className="text-2xl max-w-full text-center text-wrap">{form.title}</h2>
+                  <a href={form.link}>
+                    <h2 className="py-2 px-4 bg-blue-500 rounded-full text-xl text-white max-w-fit shadow-md hover:scale-105 duration-300">
+                      Download
+                    </h2>
+                  </a>
+                </div>
+                <div />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
