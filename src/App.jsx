9bb9c2000f660e48f3cedbd6efd9f3a88d122424
Shefa <shefaoudeen123@gmail.com>
2024-09-04 18:49:02 +0530

    cms

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ export default function App() {
           <Route path="/downloads" element={<Download />} />
           <Route path="/contactUs" element={<Contact />} />
           <Route path="/credits" element={<Credits />} />
-          <Route path=".cms" element={<Cms />} />
+          <Route path="/cms" element={<Cms />} />
         </Route>
       </Routes>
     </BrowserRouter>
